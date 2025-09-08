021c3a44335e0f11d4e46519a2cd8f904710b219
Qarajeen <qarajeen@gmail.com>
2025-09-08 18:44:03 +0400

    wrh1

---
+++ App.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Quote from './components/Quote';
+import Footer from './components/Footer';
+
+const App: React.FC = () => {
+  return (
+    <div className="bg-gradient-to-br from-[#00001c] via-[#150050] to-[#3f0071] text-white overflow-x-hidden">
+      <Hero />
+      <main>
+        <About />
+        <Services />
+        <Quote />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
